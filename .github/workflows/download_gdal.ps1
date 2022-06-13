6ef3154563dad270a0532b6b50cfee5839694e60
crazyzlj <crazyzlj@gmail.com>
2022-06-13 14:30:03 +0800

    Use crlf in .ps1 script

--- .github/workflows/download_gdal.ps1
+++ .github/workflows/download_gdal.ps1
@@ -1,6 +1,7 @@
 ﻿param ($gdalPath = "$env:SystemDrive\gdal", $VSversion = "1928", $GDALversion = "3.5.0", $MAPSversion = "7.6.4")
 $GDALversion=$GDALversion -replace '\.','-'
 $MAPSversion=$MAPSversion -replace '\.','-'
+$urllib = "https://download.gisinternals.com/sdk/downloads/release-$VSversion-x64-gdal-$GDALversion-mapserver-$MAPSversion-libs.zip"
 $urlbin = "https://download.gisinternals.com/sdk/downloads/release-$VSversion-x64-gdal-$GDALversion-mapserver-$MAPSversion.zip"
 $zipLibFile = "$gdalPath\gdallib.zip"
 $zipBinFile = "$gdalPath\gdal.zip"
@@ -12,7 +13,9 @@ if ((Test-Path -path $gdalPath) -eq $false) {
 Set-Location $gdalPath
 Write-Host "Downloading GDAL-$GDALversion-mapserver-$MAPSversion built by VS-$VSversion……"
 $webClient = New-Object System.Net.WebClient
+Write-Host $urllib
 $webClient.DownloadFile($urllib,$zipLibFile)
+Write-Host $urlbin
 $webClient.DownloadFile($urlbin,$zipBinFile)
 Expand-Archive -Path $zipLibFile -DestinationPath "$gdalPath"
 Expand-Archive -Path $zipBinFile -DestinationPath "$gdalPath"
